25d36292c1d056d85d8ea3654fce4ad943ef0151
sudharaathith <t.sudharaathith@gmail.com>
2023-05-08 22:04:38 +0530

    model url

--- DynamicBlog/settings.py
+++ DynamicBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1bq0=)7mhw8fwdkhohm6xh&2mzmau92^86m9w#k0ex61l+qmz0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
