3d6ad1bc44729cdcc4e9356f684b4260feb10b68
sudharaathith <t.sudharaathith@gmail.com>
2023-05-14 22:06:26 +0530

    ui

--- frontend/src/Pages/Edit.jsx
+++ frontend/src/Pages/Edit.jsx
@@ -33,7 +33,7 @@ function Edit(props) {
             console.log(response.data);
         })
     }
-    let [body, setBody] = useState([]); //summa
+    let [body, setBody] = useState([]);
 
     const addText = ()=>{
         const len = body.length;
